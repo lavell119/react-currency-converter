0a2428c26284b4b1a1994f97244a023734a66527
lavell119 <leewhiz119@gmail.com>
2021-11-15 23:01:09 -0500

    cleaned up boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import CurrencyRow from './CurrencyRow';
 
-function App() {
+
   return (
     <>
-      <h1>Convert</h1>
-      <CurrencyRow />
-      <div>=</div>
-      <CurrencyRow />
+
     </>
   );
 }
