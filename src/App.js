229bb7e92edc96f3a04c2252643a66328b29b789
lavell119 <leewhiz119@gmail.com>
2021-11-21 21:20:36 -0500

    imported pokemonlist and set default state in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import PokemonList from './PokemonList'
  
 function App() {
   const [pokemon, setPokemon] = useState()
+
   return (
-      null  
+      <PokemonList pokemon={pokemon} />
   );
 }
 
